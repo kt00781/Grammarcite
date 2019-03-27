a008fbad9ed855c1e1d24e5dc49be6e8d44e0fb6
DeathBringer271 <47785206+DeathBringer271@users.noreply.github.com>
2019-03-27 11:46:47 -0400

    Update StartofCapitalizationThingy.py

--- StartofCapitalizationThingy.py
+++ StartofCapitalizationThingy.py
@@ -1,7 +1,15 @@
 'Capitalizes only the first letter but that is all I have got' 
+file_r = input("Input name of txt file: ")
 
-string_name = 'how do i set up a capitalization. machines in this thing'
-
-Caps = string_name.capitalize()
+fr = open('{}.txt'.format(file_r))
+text = fr.read()
+fr.close
+Caps = text.capitalize()
 
 print(Caps)
+
+file_w = input("Input name of new file: ")
+
+fw = open('{}.txt'.format(file_w),'w')
+fw.write(separator2.join(changeline))
+fw.close
